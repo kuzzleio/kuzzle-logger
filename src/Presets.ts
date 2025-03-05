d0f10fc60c2c7c9910389211f30f2bc1d1caade4
Eric Trousset <etrousset@kuzzle.io>
2025-03-05 17:34:10 +0100

    feat:add support for levelMap on pino-loki transport
    also added default map to match the logging methods level

--- src/Presets.ts
+++ src/Presets.ts
@@ -110,6 +110,14 @@ export abstract class Presets {
               ...(transport.presetOptions.labels ?? {}),
               service_name: globalSettings.serviceName,
             },
+            levelMap: transport.presetOptions.levelMap ?? {
+              10: 'trace',
+              20: 'debug',
+              30: 'info',
+              40: 'warning',
+              50: 'error',
+              60: 'fatal',
+            },
           },
           target: 'pino-loki',
         };
